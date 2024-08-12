876d9f8cb4072e5004cd4900cf367deac079b4fc
aguespudbitrus <alejo.habbacuc@gmail.com>
2024-08-12 09:08:08 -0500

    Firebase

---
+++ src/Firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUeE8ZDCWUQTkr_hBxacZaxqNCXjo6YJ4",
+  authDomain: "todolist-ada53.firebaseapp.com",
+  projectId: "todolist-ada53",
+  storageBucket: "todolist-ada53.appspot.com",
+  messagingSenderId: "641405580408",
+  appId: "1:641405580408:web:620f1545ff6e689489e98d",
+  measurementId: "G-NXNDLRNL7M"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
